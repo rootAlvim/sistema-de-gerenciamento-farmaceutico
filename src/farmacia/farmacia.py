7f7cfbc49871d97e5160e9b7d5e42aa1be4f6d31
unknown <nicolas.raony06@gmail.com>
2025-12-26 18:54:55 -0300

    Add: class base farmacia

--- src/farmacia/farmacia.py
+++ src/farmacia/farmacia.py
@@ -1,1 +1,28 @@
 #implementacao de classe Farmacia
+from decimal import Decimal
+from core.funcionario import Funcionario
+from venda import Venda
+
+class Farmacia:
+    def __init__(self, nome : str):
+        self.nome = nome
+        self.__listaVendas = []
+        self.__funcionarios = []
+
+    def getListaVendas(self):
+        '''Retorna uma lista contendo todos os objetos de Venda registrados'''
+        return self.__listaVendas
+    
+    def criarVenda(self, id: int, funcionario: Funcionario):
+        '''Cria e retorna um objeto do tipo Venda. Adiciona obejto em Lista de Vendas'''
+        # a ser implementado
+        pass
+
+    def registrarGerente(self):
+        '''Cria um novo objeto do tipo Gerente e retorna o seu ID. Adiciona novo Gerente a lista de funcionarios'''
+        pass
+
+    def registrarAtendente(self):
+        '''Cria um novo objeto do tipo Atentende e retorna o seu ID. Adiciona novo Antende a lista de funcionarios'''
+        pass
+
