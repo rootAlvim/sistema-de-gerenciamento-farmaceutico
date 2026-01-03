c83c36470d7611d61c7bb6c7a32603113f04fdd3
unknown <nicolas.raony06@gmail.com>
2026-01-03 15:58:30 -0300

    Add: testes para farmacia

--- tests/test_farmacia.py
+++ tests/test_farmacia.py
@@ -1,20 +1,30 @@
 # usar para tests: python -m tests.test_farmacia
 
 from src.farmacia.farmacia import Farmacia
-from src.farmacia.estoque import Estoque
 from src.farmacia.produto import Produto
 
-produ1 = Produto('teste', 12.5, 'tester')
-produ2 = Produto('teste', 12.5, 'tester')
+farmacia = Farmacia("Pague mais")
+print(farmacia.getIdUnico(idParaAtendente=True)) # teste metodo getIdUnico
 
-farm = Farmacia("Pague mais")
+farmacia.registrarGerente("Teste Gerente", '598.487.125-08', '02072004', 1899) # teste registro de gerente
+print(farmacia.gerente.nome)
 
-# nao funciona, precisa finalizar estoque e atualizar os testes
-farm._estoque = Estoque()
-farm._estoque.produtos = [(produ1, 156), (produ2, 68)]
+farmacia.registrarAtendente("Teste Atendente", '64785412698', '01061999', 1550) # teste registro de Atendente
+print(farmacia.funcionarios[0].nome)
 
-#testes de atribuicao de id unico
-print(produ1.setIdUnico(farm))
-print(produ2.setIdUnico(farm))
+farmacia.criarVenda(farmacia.funcionarios[0]) # teste criar Venda
+print(farmacia.getListaVendas()[0])
 
-print(farm.getIdUnico(idParaProduto=True))
+produto1 = Produto("Teste Produto", 14.5, "Tester")
+produto1.setIdUnico(farmacia) # teste de set id para produto
+print(produto1.getId()) 
+
+farmacia._estoque.adicionar_produto(produto1, 168) # teste adicionar produto
+print(farmacia._estoque.get_produtos())
+
+produto2 = Produto("Teste Produto 2", 19.5, "Tester2")
+produto2.setIdUnico(farmacia) # teste de set id para produto quando já existe mais de um em estoque
+print(produto2.getId()) 
+
+farmacia._estoque.adicionar_produto(produto2, 308) # teste adicionar produto 2
+print(farmacia._estoque.get_produtos())
