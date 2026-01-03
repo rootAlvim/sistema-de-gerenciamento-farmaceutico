e9ba61c4d824aae71cacd696b8d76a3c309d7481
unknown <nicolas.raony06@gmail.com>
2026-01-03 15:57:29 -0300

    Add: __repr__ | Update: setIdUnico

--- src/farmacia/produto.py
+++ src/farmacia/produto.py
@@ -1,5 +1,3 @@
-from src.farmacia.farmacia import Farmacia
-
 class Produto:
     def __init__(self, nome, preco, fabricante):
         self.__id = None
@@ -10,11 +8,19 @@ class Produto:
 
     def getId(self):
         return self.__id
+    
     def getPreco(self):
         return self.__preco
     
-    def setIdUnico(self, farmacia: Farmacia):
+    def setIdUnico(self, farmacia):
+        from src.farmacia.farmacia import Farmacia
+        if not isinstance(farmacia, Farmacia):
+            raise ValueError("Parametro farmacia deve ser uma instacia do tipo Farmacia")
+        
         if not self.__id:
             self.__id = farmacia.getIdUnico(idParaProduto=True)
             return self.getId()
         raise ValueError("Id de Produto so pode ser alterado apenas uma vez")
+    
+    def __repr__(self):
+        return f'Produto({self.nome}, {self.__preco}, {self.fabricante})'
