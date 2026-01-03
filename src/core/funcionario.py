f54ecfb4ad72ff7d656724112c71f0f80b9148be
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-03 14:41:22 -0300

    Update funcionario.py

--- src/core/funcionario.py
+++ src/core/funcionario.py
@@ -1,8 +1,10 @@
 #implementacao de classe abstrata Funcionario
 from datetime import datetime
 from src.core.pessoa import Pessoa
-class Funcionario(Pessoa):
-    def __init__(self,nome:str,cpf:str,data_nascimento:datetime,salario_base:float,id:int, cargo):
+from Mixins_Interfaces.adicionar_produto import Adicionar_ProdutoMixin
+from Mixins_Interfaces.vender_produto import Vender_ProdutoMixin
+class Funcionario(Pessoa,Adicionar_ProdutoMixin,Vender_ProdutoMixin):
+    def __init__(self,nome:str,cpf:str,data_nascimento:datetime,salario_base:float,id:int, cargo:str):
         super().__init__(nome,cpf,data_nascimento)
         self.__salario_base = salario_base
         self.__id = id
@@ -23,42 +25,11 @@ class Funcionario(Pessoa):
         '''Retorna True para autenticado False para não autenticado'''
         return self.__autenticado
     
-    def adicionar_produto(self, produto, quantidade,estoque):
-        produtos_estoque = estoque.get_produtos()
-        if produto.id in produtos_estoque:
-            produtos_estoque[produto.id]["quantidade"] += quantidade
-        else:
-            produtos_estoque[produto.id] = {
-                "produto": produto,
-                "quantidade": quantidade
-            }
-
-    def vender_produto(self, id_produto, quantidade,estoque):
-        produtos_estoque = estoque.get_produtos()
-        if id_produto in produtos_estoque:
-            if produtos_estoque[id_produto]["quantidade"] >= quantidade:
-                produtos_estoque[id_produto]["quantidade"] -= quantidade
-                return True
-            else:
-                print('Quantidade insuficiente')
-        else:
-            print('Produto não encontrado.')
-        return False
-    
-    def consultar(self,estoque):
-        if not estoque.get_produtos():
-            return "Estoque vazio"
-
-        return {
-        
-            dados["produto"].nome: dados["quantidade"]
-            for dados in estoque.get_produtos().values()
-        }
     def subTotal(self, estoque):
         produtos_estoque = estoque.get_produtos()
-        '''total = 0
+        total = 0
 
-        for registro in produtos_estoque.values():
+        '''for registro in produtos_estoque.values():
             produto = registro["produto"]
             quantidade = registro["quantidade"]
             total += produto.preco * quantidade
@@ -69,3 +40,15 @@ class Funcionario(Pessoa):
             quantidade = dados["quantidade"]
             subtotal = produto.preco * quantidade
             print(f"{produto.nome} - R$ {subtotal:.2f}")
+    
+    def consultar(self,estoque):
+        if not estoque.get_produtos():
+            return "Estoque vazio"
+
+        return {
+        
+            dados["produto"].nome: dados["quantidade"]
+            for dados in estoque.get_produtos().values()
+        } 
+    
+
