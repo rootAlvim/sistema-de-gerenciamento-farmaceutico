0b06cff0d172310484f8f0422ef50474217f789a
unknown <nicolas.raony06@gmail.com>
2026-01-04 23:39:11 -0300

    Update: testes para nova geracao de id

--- tests/test_farmacia.py
+++ tests/test_farmacia.py
@@ -4,26 +4,23 @@ from src.farmacia.farmacia import Farmacia
 from src.farmacia.produto import Produto
 
 farmacia = Farmacia("Pague mais")
-print(farmacia.getIdUnico(idParaAtendente=True)) # teste metodo getIdUnico
 
 farmacia.registrarGerente("Teste Gerente", '598.487.125-08', '02072004', 1899) # teste registro de gerente
-print(farmacia.gerente.nome)
+print(farmacia.gerente.get_id())
 
 farmacia.registrarAtendente("Teste Atendente", '64785412698', '01061999', 1550) # teste registro de Atendente
-print(farmacia.funcionarios[0].nome)
+print(farmacia.funcionarios[0].get_id())
 
 farmacia.criarVenda(farmacia.funcionarios[0]) # teste criar Venda
-print(farmacia.getListaVendas()[0])
+print(farmacia.getListaVendas()[0].getId())
 
 produto1 = Produto("Teste Produto", 14.5, "Tester")
-produto1.setIdUnico(farmacia) # teste de set id para produto
 print(produto1.getId()) 
 
 farmacia._estoque.adicionar_produto(produto1, 168) # teste adicionar produto
 print(farmacia._estoque.get_produtos())
 
 produto2 = Produto("Teste Produto 2", 19.5, "Tester2")
-produto2.setIdUnico(farmacia) # teste de set id para produto quando já existe mais de um em estoque
 print(produto2.getId()) 
 
 farmacia._estoque.adicionar_produto(produto2, 308) # teste adicionar produto 2
--- tests/test_produto.py
+++ tests/test_produto.py
@@ -1,11 +1,17 @@
 # teste de implementacoes 
 # python -m tests.test_produto
 from src.farmacia.produto import Produto
-produto_teste = Produto(1,'Amitril',2.90,'Cimed')
+
+produto_teste = Produto('Amitril',2.90,'Cimed')
+print(produto_teste.getId()) # teste de gerador de id
+
+produto_teste2 = Produto('Amitril',2.90,'Cimed')
+print(produto_teste2.getId()) # teste de gerador de id
+
 print(50*'-')
 print('PRODUTO-TESTE')
-print(f'ID: {produto_teste.id}')
+print(f'ID: {produto_teste.getId()}')
 print(f'NOME: {produto_teste.nome}')
-print(f'PREÇO: {produto_teste.preco}')
+print(f'PREÇO: {produto_teste.getPreco()}')
 print(f'FABRICANTE: {produto_teste.fabricante}')
 print(50*'-')
