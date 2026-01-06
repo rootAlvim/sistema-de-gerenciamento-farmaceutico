941d5b95252e95473f9541cb266264b43f03f76a
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-05 23:22:29 -0300

    Update adicionar_produto.py

--- src/core/mixins_interfaces/adicionar_produto.py
+++ src/core/mixins_interfaces/adicionar_produto.py
@@ -2,5 +2,5 @@ class Adicionar_ProdutoMixin:
     def adicionar_produto_estoque(self, produto, quantidade, estoque_obj):
         """Adiciona qtd ao estoque chamando o metodo do obj Estoque"""
         estoque_obj.adicionar_produto(produto, quantidade)
-        print(f"[{self.cargo}] Adicionado {quantidade}x {produto.nome}")
+        print(f"[{self.__class__.__name__}] Adicionado {quantidade}x {produto.nome}")
     
