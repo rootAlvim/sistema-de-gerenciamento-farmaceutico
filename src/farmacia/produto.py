cf84b4497d20d2d424be34cd497533aef3a763fd
unknown <nicolas.raony06@gmail.com>
2026-01-06 18:12:35 -0300

    Update: metodo setPreco|__repr__|__init__

--- src/farmacia/produto.py
+++ src/farmacia/produto.py
@@ -1,19 +1,33 @@
 from src.utils.gerador_id import getIdProduto
+from decimal import Decimal
 
 class Produto:
     allIds = []
-    def __init__(self, nome, preco, fabricante):
-        self.__id = getIdProduto(self)
+    def __init__(self, nome : str, preco : Decimal, fabricante : str, id : int = None, logAlteracoes : list = None):
+        self.__id = id if id else getIdProduto(self)
         self.nome = nome
         self.__preco = preco
         self.fabricante = fabricante
-        self.__logAlteracoes = []
+        self.__logAlteracoes = logAlteracoes if logAlteracoes else []
 
     def getId(self):
+        '''Retorna o Id de Produto.'''
         return self.__id
     
     def getPreco(self):
+        '''Retorna o preco de Produto.'''
         return self.__preco
     
+    def setPreco(self, preco : Decimal, gerente):
+        '''Recebe como parametro o novo valor de preco em Decimal e um objeto de Gerente. Altera o preco de Produto.'''
+        from src.core.gerente import Gerente
+        if not isinstance(gerente, Gerente):
+            raise ValueError('Metodo deve receber um objeto do tipo Gerente')
+        
+        if preco < 0:
+            raise ValueError('Preco deve ser maior que 0')
+        
+        self.__preco = preco
+    
     def __repr__(self):
-        return f'ID: {self.__id} | Nome: {self.nome} | Preço: {self.__preco} | Fabricante: {self.fabricante}'
+        return f'Produto({self.nome}, {self.__preco}, {self.fabricante}, {self.__id}, {self.__logAlteracoes})'
