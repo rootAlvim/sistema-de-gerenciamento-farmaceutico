ffe932da940ba35e1c8394f80b621cb265f2984b
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-05 23:22:38 -0300

    Update atendente.py

--- src/core/atendente.py
+++ src/core/atendente.py
@@ -1,10 +1,6 @@
 #implementacao de classe Atendente
 from src.core.funcionario import Funcionario
-from src.utils.gerador_id import getIdProduto
 
 class Atendente(Funcionario):
-    allIds = []
     def __init__(self, nome, cpf, data_nascimento, salario_base, id: int):
         super().__init__(nome, cpf, data_nascimento, salario_base, id)
-
-    
