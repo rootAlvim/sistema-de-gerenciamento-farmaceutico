d10486ba6e4d3474406034273d810eb45529cd2d
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-05 23:22:35 -0300

    Update cliente.py

--- src/core/cliente.py
+++ src/core/cliente.py
@@ -2,10 +2,12 @@
 from src.core.pessoa import Pessoa
 from datetime import datetime
 class Cliente(Pessoa):
-    def __init__(self,nome:str,cpf:str,data_nascimento:datetime, id_cliente):
+    def __init__(self,nome:str,cpf:str, id_cliente:int,data_nascimento=None,):
         super().__init__(nome,cpf,data_nascimento)
         self.__id_cliente = id_cliente
 
     def get_id_cliente (self):
         self.get_cpf = self.__id_cliente
         return self.__id_cliente
+    def __str__(self):
+        return f'Nome: {self.nome} | Id: {self.get_id_cliente()}'
