223fdc5e073bad281ce2925a26372cd723136513
unknown <nicolas.raony06@gmail.com>
2026-01-10 14:38:43 -0300

    Add: registro de cliente | getters

--- src/farmacia/farmacia.py
+++ src/farmacia/farmacia.py
@@ -1,8 +1,5 @@
 #implementacao de classe Farmacia
 from decimal import Decimal
-from src.core.atendente import Atendente
-from src.core.gerente import Gerente
-from src.farmacia.venda import Venda
 from src.farmacia.estoque import Estoque
 from src.utils.validacoes import validar_funcionario
 from random import randint
@@ -14,6 +11,7 @@ class Farmacia:
         self._estoque = Estoque()
         self.gerente = None
         self.funcionarios = []
+        self.__clientes = []
         self.__idGerentes = 0
         self.__idAtendentes = 0
         self.__idVendas = 0
@@ -22,8 +20,23 @@ class Farmacia:
         '''Retorna uma lista contendo todos os objetos de Venda registrados'''
         return self.__listaVendas
     
+    def getClientes(self):
+        '''Retorna uma lista contendo todos os objetos de Cliente registrados'''
+        return self.__clientes
+    
+    def getClientePorCpf(self, cpf):
+        '''Retorna objeto de cliente caso exista um com o mesmo cpf recebido.'''
+        import re
+        cpf = re.sub(r'\D', '', cpf)
+        
+        for cliente in self.__clientes:
+            cpf_cliente = re.sub(r'\D', '', cliente.get_cpf())
+            if cpf == cpf_cliente:
+                return cliente
+
     def criarVenda(self, funcionario):
         '''Cria um objeto do tipo Venda. Adiciona obejto em Lista de Vendas e retorna seu indice'''
+        from src.farmacia.venda import Venda
         validar_funcionario(funcionario)
         self.__idVendas += 1
         venda = Venda(self.__idVendas, funcionario)
@@ -33,15 +46,24 @@ class Farmacia:
      
     def registrarGerente(self, nome, cpf, data_nasc, salario):
         '''Recebe como parametros atributos de um Gerente e cria um novo objeto do tipo Gerente.'''
+        from src.core.gerente import Gerente
         self.__idGerentes += 1
         self.gerente = Gerente(nome, cpf, data_nasc, salario, self.__idGerentes)
         
     def registrarAtendente(self, nome, cpf, data_nasc, salario):
         '''Recebe como parametros atributos de um Atendente e cria um novo objeto do tipo Atendente. Retorna seu indice na lista de funcionarios.'''
+        from src.core.atendente import Atendente
         self.__idAtendentes += 1
         atendente = Atendente(nome, cpf, data_nasc, salario, self.__idAtendentes)
         
         self.funcionarios.append(atendente)
         return self.funcionarios.index(atendente)
+    
+    def registrarCliente(self, nome : str, cpf : str, data_nascimento = None):
+        '''Recebe como parametros atributos de um Cliente e cria um novo objeto do tipo Cliente. Retorna seu indice na lista de clientes.'''
+        from src.core.cliente import Cliente
+        cliente = Cliente(nome, cpf, data_nascimento)
 
+        self.__clientes.append(cliente)
+        return self.__clientes.index(cliente)
 
