750b9c07d22b1fde66b01dfebd0ef8313d84d9e0
unknown <nicolas.raony06@gmail.com>
2026-01-13 18:39:27 -0300

    Add: mixin de registro de cliente

--- src/core/funcionario.py
+++ src/core/funcionario.py
@@ -2,10 +2,11 @@
 from datetime import datetime
 from abc import abstractmethod
 from src.core.pessoa import Pessoa
-from src.core.mixins_interfaces.adicionar_produto import Adicionar_ProdutoMixin
+from src.core.mixins_interfaces.adicionar_produto import AdicionarProdutoMixin
 from src.core.mixins_interfaces.gerenciar_venda import GerenciarVendaMixin
+from src.core.mixins_interfaces.registrar_cliente import RegistrarClienteMixin
 
-class Funcionario(Pessoa,Adicionar_ProdutoMixin,GerenciarVendaMixin):
+class Funcionario(Pessoa, AdicionarProdutoMixin, GerenciarVendaMixin, RegistrarClienteMixin):
     def __init__(self, nome:str, cpf:str, data_nascimento:datetime, salario_base:float, id:int):
         super().__init__(nome,cpf,data_nascimento)
         self.__salario_base = salario_base
