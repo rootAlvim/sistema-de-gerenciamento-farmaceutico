079fcca90eba19841228ad5eaf9776baeb72047e
unknown <nicolas.raony06@gmail.com>
2026-01-13 14:09:56 -0300

    Add: mixin GerenciarVendaMixin

---
+++ src/core/mixins_interfaces/gerenciar_venda.py
@@ -0,0 +1,22 @@
+class GerenciarVendaMixin:
+    def registrar_venda(self, farmacia):
+        '''Registra nova venda em farmacia. Retorna o Id da venda.'''
+        id = farmacia.criarVenda(self)
+        return id
+    
+    def adicionar_produto_venda(self, farmacia, produto, quantidade):
+        '''Adiciona produto em venda, ultima realizada por funcionario, caso produto esteja disponivel no estoque.'''
+        estoque = farmacia._estoque
+        if estoque.produto_disponibilidade(produto, quantidade):
+            self.getVendasRealizadas()[-1].adicionarProduto(produto, quantidade)
+            estoque.remover_produto(produto.getId(), quantidade)
+            return True
+
+    def adicionar_cliente_venda(self, cliente):
+        '''Adiciona cliente em ultima venda registrada pelo funcionario.'''
+        self.getVendasRealizadas()[-1].adicionarCliente(self, cliente)
+
+    def finalizar_venda(self):
+        '''Finaliza ultima venda realizada pelo funcionario.'''
+        self.getVendasRealizadas()[-1].setPrecoTotal(self)
+        
--- src/core/mixins_interfaces/vender_produto.py
+++
@@ -1,4 +0,0 @@
-class Vender_ProdutoMixin:
-    def vender_produto(self, id_produto, quantidade, estoque_obj):
-        """Realiza a baixa no estoque chamando o metodo do obj estoque"""
-        estoque_obj.remover_produto(id_produto, quantidade)
