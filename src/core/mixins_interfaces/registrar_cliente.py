f283398b1ea18bae197d05fb16dc89833384c1ff
unknown <nicolas.raony06@gmail.com>
2026-01-19 10:03:20 -0300

    Fix: mixins

--- src/core/mixins_interfaces/registrar_cliente.py
+++ src/core/mixins_interfaces/registrar_cliente.py
@@ -1,3 +1,3 @@
 class RegistrarClienteMixin:
     def registrar_cliente(self, nome: str, cpf: str, data_nascimento = None):
-        self.__farmacia._registrarCliente(self, nome, cpf, data_nascimento)
+        self.getFarmacia()._registrarCliente(self, nome, cpf, data_nascimento)
