9bf4861fd0f2cf57fd35dab0e6946d6d078099d0
Nicolas Raony <nicolas.raony06@gmail.com>
2026-01-20 16:13:31 -0300

    Fix: mixin gerenciar estoque

--- src/core/mixins_interfaces/gerenciar_estoque.py
+++ src/core/mixins_interfaces/gerenciar_estoque.py
@@ -2,31 +2,29 @@ class GerenciarEstoqueMixin:
     def adicionar_produto_estoque(self,produto , quantidade):
         """Adiciona qtd ao estoque chamando o metodo do obj Estoque"""
         estoque = self.getFarmacia()
-        estoque._estoque.adicionar_produto(produto, quantidade)
+        estoque._estoque.adicionar_produto(self, produto, quantidade)
         
     def remover_produto(self, id, quantidade = None):
        '''Recebe um inteiro do Id de Produto e, remove ou reduz sua quantidade do estoque, caso parametro 'quantidade' tenha um valor inteiro positivo.'''
        estoque = self.getFarmacia()
-       estoque.remover_produto(id,quantidade)
+       estoque.remover_produto(self, id, quantidade)
             
     def consultar_estoque(self):
         '''Retorna um dicionario contendo todos os produtos e suas quantidades em estoque.'''
         estoque = self.getFarmacia()._estoque
-        if not estoque.get_produtos():
-            return False
 
         return {
         
             dados["produto"]: dados["quantidade"]
-            for dados in estoque.get_produtos().values()
+            for dados in estoque.get_produtos(self).values()
         }
     
     def consultar_produto_por_id(self, id_produto):
         '''Recebe id do produto e retorna seu objeto e quantidade em estoque.'''
         estoque = self.getFarmacia()._estoque
-        return estoque.consultar_produto_por_id(id_produto)
+        return estoque.consultar_produto_por_id(self, id_produto)
 
     def consultar_produto_por_nome(self, nome ):
         '''Recebe nome do produto e retorna seu objeto e quantidade em estoque.'''
         estoque = self.getFarmacia()._estoque
-        return estoque.consultar_produto_por_nome(nome)
+        return estoque.consultar_produto_por_nome(self, nome)
